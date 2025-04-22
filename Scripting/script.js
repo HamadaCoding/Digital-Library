48ba49bdd8e62bb69fb8941a9e8538a397a825bb
HamadaCoding <138389818+HamadaCoding@users.noreply.github.com>
2025-04-22 00:32:52 +0000

    ...

--- Scripting/script.js
+++ Scripting/script.js
@@ -1,51 +1,51 @@
 function home() {
-    window.location = "../index.html"   
+    window.location = "/index.html"   
 }
 
 function books() {
-    window.location = "../Html/books.html"
+    window.location = "/Html/books.html"
 }
 
 function feedback() {
-    window.location = "../Html/feedback.html"
+    window.location = "/Html/feedback.html"
 }
 
 function contact() {
-    window.location = "../Html/contact.html"
+    window.location = "/Html/contact.html"
 }
 
 function curriculum() {
-    window.location = "Curriculum/curriculum.html"
+    window.location = "/Curriculum/curriculum.html"
 }
 
 function horror() {
-    window.location = "Horror/horror.html"
+    window.location = "/Horror/horror.html"
 }
 
 function romantic() {
-    window.location = "Romantic/romantic.html"
+    window.location = "/Romantic/romantic.html"
 }
 
 function comic() {
-    window.location = "Comic/comic.html"
+    window.location = "/Comic/comic.html"
 }
 
 function adventure() {
-    window.location = "Adventure/adventure.html"
+    window.location = "/Adventure/adventure.html"
 }
 
 function mystery() {
-    window.location = "Mystery/mystery.html"
+    window.location = "/Mystery/mystery.html"
 }
 
 function thriller() {
-    window.location = "Thriller/thriller.html"
+    window.location = "/Thriller/thriller.html"
 }
 
 function fiction() {
-    window.location = "Fiction/fiction.html"
+    window.location = "/Fiction/fiction.html"
 }
 
 function philosophical() {
-    window.location = "Philosophical/philosophical.html"
+    window.location = "/Philosophical/philosophical.html"
 }
