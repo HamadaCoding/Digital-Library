54f1b78d2dcd53e60be9e8bded98540cb32812bd
HamadaCoding <138389818+HamadaCoding@users.noreply.github.com>
2025-04-22 02:21:59 +0200

    Add files via upload

---
+++ Digital Library/Scripting/script.js
@@ -0,0 +1,51 @@
+function home() {
+    window.location = "../index.html"   
+}
+
+function books() {
+    window.location = "../Html/books.html"
+}
+
+function feedback() {
+    window.location = "../Html/feedback.html"
+}
+
+function contact() {
+    window.location = "../Html/contact.html"
+}
+
+function curriculum() {
+    window.location = "Curriculum/curriculum.html"
+}
+
+function horror() {
+    window.location = "Horror/horror.html"
+}
+
+function romantic() {
+    window.location = "Romantic/romantic.html"
+}
+
+function comic() {
+    window.location = "Comic/comic.html"
+}
+
+function adventure() {
+    window.location = "Adventure/adventure.html"
+}
+
+function mystery() {
+    window.location = "Mystery/mystery.html"
+}
+
+function thriller() {
+    window.location = "Thriller/thriller.html"
+}
+
+function fiction() {
+    window.location = "Fiction/fiction.html"
+}
+
+function philosophical() {
+    window.location = "Philosophical/philosophical.html"
+}
